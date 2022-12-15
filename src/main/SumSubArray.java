10a5f305784666df208ec76e72dabb7694a5f907
Tiffany678 <52tiffany.yang@gmail.comgit config --global user.name Tiffany678git config --global user.email 52tiffany.yang@gmail.com>
2022-12-14 21:50:25 -0500

    complete

--- src/main/SumSubArray.java
+++ src/main/SumSubArray.java
@@ -3,14 +3,14 @@ package main;
 import java.util.Arrays;
 
 public class SumSubArray {
-    //This method will return all the possibilities of subArray in an Array
+    //This method will return the sum of all the combination in an Array
     public int sumSubArray(int[] intArr){
 
         int sum =0;
-
         for(int i=0; i<intArr.length; i++){
             int[] newArr = Arrays.copyOfRange(intArr,i,intArr.length);
             int count = newArr.length;
+
             for(int j=0; j<newArr.length; j++){
                 sum+= newArr[j]*count;
                 count--;
